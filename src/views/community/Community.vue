2b7bb3819c894ecadace1ca97af06514292a9a45
codehly <1900561256@qq.com>
2021-07-13 17:45:54 +0800

    tow

--- src/views/community/Community.vue
+++ src/views/community/Community.vue
@@ -7,12 +7,28 @@
     <div slot="center">蘑菇社区</div>
     </NavBar>
   <input type="file" name="picture" ref="file" @change="upload"> 
+  <mt-button type="primary" size="large" @click="startUpload">上传</mt-button>
   <ul class="contains">
       <li v-for="item in vedio">
           <img v-if="item.type.startsWith('image')" :src="item.fileUrl">
           <video v-else :src="item.fileUrl" controls="controls"></video>
           </li>
   </ul>
+  <mt-navbar v-model="selected">
+  <mt-tab-item id="1">图片</mt-tab-item>
+  <mt-tab-item id="2">视频</mt-tab-item>
+</mt-navbar>
+
+<!-- tab-container -->
+<mt-tab-container v-model="selected">
+  <mt-tab-container-item id="1">
+   内容1
+  </mt-tab-container-item>
+  <mt-tab-container-item id="2">
+   内容2
+  </mt-tab-container-item>
+</mt-tab-container>
+
   </div>
 </template>
 
@@ -20,14 +36,22 @@
 import axios from "axios";
 import {getMoment,getVedio} from "network/profile"
 import NavBar from "components/common/navbar/NavBar";
+import { Navbar, TabItem,TabContainer, TabContainerItem,Button,Toast } from 'mint-ui';
 export default {
   name:'Community',
   components:{
-      NavBar
+      NavBar,
+      [Navbar.name]:Navbar,
+      [TabItem.name]:TabItem,
+      [TabContainer.name]:TabContainer,
+      [TabContainerItem.name]:TabContainerItem,
+      [Button.name]:Button
   },
   data(){
       return {
-          vedio:null
+          vedio:null,
+          selected:1,
+          formData:null
       }
   },
   async created(){
@@ -40,36 +64,28 @@ export default {
        back() {
       this.$router.back();
     },
-    upload(a){
-        // var file = this.$refs.file.files[0];
-        // var data = new FormData();
-        // data.append("file",file);
-        const file=a.target.files[0];
-        let formData = new FormData();
-        formData.append('picture',file);
-        let instance=axios.create({
+    startUpload(){
+      const formData=this.formData;
+      let instance=axios.create({
             baseURL:'http://120.79.146.219:8000',
             headers:{"Content-Type":"multipart/form-data",
                         Authorization:window.sessionStorage.getItem('token')
-            }
+            },
+            onUploadProgress(progressEvent){
+               if (progressEvent.lengthComputable) {
+                 if(progressEvent.loaded==progressEvent.total){
+                   Toast("上传成功");
+                 }
+                  
+              }
+      }
         })
         instance.post('/upload/picture?momentId=2',formData).then(res=>{console.log(res)})
-        // axios({
-        //     url:'http://120.79.146.219:8000/upload/picture?momentId=2',
-        //     method:'post',
-        //     data:{
-        //         formData
-        //     },
-        //     headers:{
-        //         'Content-Type':'multipart/form-data',
-        //         Authorization:window.sessionStorage.getItem('token')
-        //     }
-        // }).then(res=>{
-        //     console.log(res);
-        // })
-        // console.log(data);
-        // console.log(a);
-        // console.log("ASas",data)
+    },
+    upload(a){
+        const file=a.target.files[0];
+        this.formData = new FormData();
+        this.formData.append('picture',file);
     }
   }
 }
